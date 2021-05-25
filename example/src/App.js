5affed7bb60ce420f6da1b32731d53e102b3444e
Jon Ambas <jon@jonambas.com>
2021-05-24 21:09:08 -0400

    Setup project

---
+++ example/src/App.js
@@ -0,0 +1,8 @@
+import nobs from "nobs";
+
+function App() {
+  console.log(nobs("test"));
+  return <div className="App">test</div>;
+}
+
+export default App;
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+type TestTypes = {
+  str: string
+}
+
+export default function test(str: TestTypes): string {
+  return str + '!!'
+}
